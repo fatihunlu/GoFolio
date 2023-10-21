bc7a3307415330f597cb706619a14b15a6ef8041
Fatih Ünlü <unlu.fa@gmail.com>
2023-10-21 15:00:40 +0300

    add StockTable component

---
+++ web/components/StockTable.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="block w-full overflow-x-auto">
+        <table class="items-center bg-transparent w-full border-collapse ">
+            <thead>
+                <tr>
+                    <th
+                        class="px-6 bg-[#f7f9f9] text-[#617d8b] align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                        Page name
+                    </th>
+                    <th
+                        class="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                        Visitors
+                    </th>
+                    <th
+                        class="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                        Unique users
+                    </th>
+                    <th
+                        class="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                        Bounce rate
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="i in 5">
+                    <th
+                        class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700 ">
+                        /argon/
+                    </th>
+                    <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 ">
+                        4,569
+                    </td>
+                    <td class="border-t-0 px-6 align-center border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                        340
+                    </td>
+                    <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                        <i class="fas fa-arrow-up text-emerald-500 mr-4"></i>
+                        46,53%
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+  
+<script>
+
+
+export default {
+    name: 'StockTable'
+}
+</script>
+  
--- web/pages/index.vue
+++ web/pages/index.vue
@@ -1,11 +1,37 @@
 <template>
-  <div>
-    <Tutorial/>
+  <div class="h-screen">
+    <div class="h-full grid grid-cols-2 items-center">
+      <div class="justify-center">
+        <div class="m-20">
+          <stock-table/>
+        </div>
+      </div>
+      <div class="justify-center">
+        <div class="m-20">
+          <stock-table/>
+        </div>
+      </div>
+      <div class="justify-center">
+        <div class="m-20">
+          <stock-table/>
+        </div>
+      </div>
+      <div class="justify-center">
+        <div class="m-20">
+          <stock-table/>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import StockTable from '../components/StockTable.vue'
+
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+    components: [
+        StockTable  
+    ]
 }
 </script>
