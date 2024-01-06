1278795c054fc9482e093b6d322f7ad14e46087f
Fatih Ünlü <unlu.fa@gmail.com>
2024-01-06 19:15:52 +0300

    allow cors

--- server.go
+++ server.go
@@ -4,10 +4,15 @@ import (
 	"gofolio/api"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	e := echo.New()
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"http://localhost:3000"},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+	}))
 
 	root := e.Group("")
 	api.InitRoutes(root)
